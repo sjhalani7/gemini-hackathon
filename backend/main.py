d237071e7fba36e75cab843823057994b50f4288
sjhalani7 <86908164+sjhalani7@users.noreply.github.com>
2024-07-21 17:08:44 -0700

    implementation to start chat on test pdf

--- backend/main.py
+++ backend/main.py
@@ -7,9 +7,27 @@ genai.configure(api_key=API_KEY)
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-def create_prompt():
-    response = model.generate_content("Teach me about how an LLM works")
-    return response.text
+def create_prompt(path_to_file):
+    material_text = f"{read_pdf(path_to_file)}"
+    # Initial prompt combining material and instructions
+    initial_prompt = f"""
+    You are an advanced language model that has been trained with the following material:
+    "{material_text}"
+    Please use this material to answer any questions based on it. If the material does not cover the question, respond that the information is not available in the provided material.
+    """
+    return initial_prompt
+
+def init_prompt_llm(prompt):
+    history = [
+        {"role": "user",
+         "parts": f"{prompt}"},
+    ]
+    chat = model.start_chat(history=history)
+    return chat
+
+def send_question_to_model(chat_session, query):
+    response = chat_session.send_message(query)
+    print(response.text)
 
 def read_pdf(file_path):
     text = ""
@@ -22,4 +40,4 @@ def read_pdf(file_path):
         print(f"Error reading PDF file: {e}")
     return text
 
-print("hello world")
+send_question_to_model(init_prompt_llm(create_prompt("backend/184 Final Report.pdf")),"What was the project on?")
