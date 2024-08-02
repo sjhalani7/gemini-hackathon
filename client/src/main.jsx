7a0073874b21cee952dd4eb05361a63bb8599c89
Christopher Tamayo <chriswtamayo@gmail.com>
2024-08-01 21:13:24 -1000

    Skeleton login page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import LoginPage from './views/LoginPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ client/src/views/LoginPage.jsx
@@ -0,0 +1,14 @@
+import '../styles/LoginPage.css'
+
+const LoginPage = () => {
+  return (
+    <div className='flex flex-col justify-center container'>
+      <div className="m-20">
+        <h1>Gem.edu</h1>
+        <p>Your personal teacher and advisor outside of the classroom.</p>
+      </div>
+    </div>
+  )
+}
+
+export default LoginPage
