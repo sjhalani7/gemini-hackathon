05f0450eeda196d00b1d032fdb553207022bbe6e
Christopher Tamayo <chriswtamayo@gmail.com>
2024-08-01 22:38:04 -1000

    Hide sensitive firebase information

--- client/src/auth/firebaseConfig.jsx
+++ client/src/auth/firebaseConfig.jsx
@@ -3,13 +3,13 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDweyXw33I_6zX6ofY2cLwCuEQ0gbt2cTo",
-  authDomain: "gem-edu.firebaseapp.com",
-  projectId: "gem-edu",
-  storageBucket: "gem-edu.appspot.com",
-  messagingSenderId: "678770855959",
-  appId: "1:678770855959:web:c510b05bc7d8c0e1d5cc52",
-  measurementId: "G-GBR1TH1Z59"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
