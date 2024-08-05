e3f5af55c9b36370ca313a7d71f60a6bd4397e86
Christopher Tamayo <chriswtamayo@gmail.com>
2024-08-04 17:12:22 -1000

    Increase width size

--- client/src/views/CourseSelectionPage.jsx
+++ client/src/views/CourseSelectionPage.jsx
@@ -33,10 +33,10 @@ const CourseSelectionPage = () => {
   );
 
   return (
-    <div className="course-selection-container flex flex-col items-center h-full w-full">
+    <div className="course-selection-container flex flex-col justify-center items-center h-full w-full">
       <p className="selection-title mt-24 mb-2">Select a course</p>
       <p className="mb-2">Term: Fall 2024</p>
-      <div className="input-container w-1/4 relative mb-2">
+      <div className="input-container w-1/2 relative mb-2">
         <input
           type="text"
           placeholder="ex. CSEN 122 - Computer Architecture"
@@ -46,9 +46,9 @@ const CourseSelectionPage = () => {
         />
         <span className="material-symbols-outlined search-icon">search</span>
       </div>
-      <div className="course-list flex flex-col w-1/4 mb-24">
+      <div className="course-list flex flex-col w-1/2 mb-24">
         {filteredCourses.map((course, index) => (
-          <button key={index} className="course-item w-full p-2 mb-0.5">
+          <button key={index} className="course-item w-full p-2 mb-2">
             {course}
           </button>
         ))}
