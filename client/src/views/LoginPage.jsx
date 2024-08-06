d1ee07614b012d27b88e3a000576cf5962a666e0
Christopher Tamayo <chriswtamayo@gmail.com>
2024-08-06 01:10:04 -1000

    Modify home page logo

--- client/src/views/LoginPage.jsx
+++ client/src/views/LoginPage.jsx
@@ -5,7 +5,7 @@ import { auth } from '../auth/firebaseConfig';
 import SignInButton from '../components/SignInButton';
 import SignOutButton from '../components/SignOutButton';
 import { useNavigate } from 'react-router-dom';
-import logo from '../assets/logo.jpg';
+import logo from '../assets/logo.png';
 
 const LoginPage = () => {
   const [user] = useAuthState(auth);
@@ -16,7 +16,7 @@ const LoginPage = () => {
   }
 
   return (
-    <div className='flex flex-row w-full h-full'>
+    <div className='flex flex-row w-full h-full overflow-hidden relative'>
       <div className='flex flex-col justify-center container'>
         <div className="home-container m-40">
           <h1>Gem.edu</h1>
@@ -24,9 +24,7 @@ const LoginPage = () => {
           {user ? <SignOutButton /> : <SignInButton />}
         </div>
       </div>
-      <div className='flex justify-center items-center w-full'>
-        <img className="logo" src={logo} alt="Logo" />
-      </div>
+      <img className="logo absolute" src={logo} alt="Logo" />
     </div>
   );
 };
