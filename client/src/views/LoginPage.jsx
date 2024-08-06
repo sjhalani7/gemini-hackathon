52e2a204f13152cd462f332deb34521b45a67f4e
Christopher Tamayo <chriswtamayo@gmail.com>
2024-08-05 15:14:23 -1000

    Add temp logo

--- client/src/views/LoginPage.jsx
+++ client/src/views/LoginPage.jsx
@@ -5,21 +5,27 @@ import { auth } from '../auth/firebaseConfig';
 import SignInButton from '../components/SignInButton';
 import SignOutButton from '../components/SignOutButton';
 import { useNavigate } from 'react-router-dom';
+import logo from '../assets/logo.jpg';
 
 const LoginPage = () => {
-  const [user] = useAuthState(auth); 
+  const [user] = useAuthState(auth);
   const navigate = useNavigate();
 
   if (user) {
     navigate("/gemini-selection");
   }
-  
+
   return (
-    <div className='flex flex-col justify-center container'>
-      <div className="home-container m-40">
-        <h1>Gem.edu</h1>
-        <p>Your personal teacher and advisor outside of the classroom.</p>
-        {user ? <SignOutButton /> : <SignInButton />}
+    <div className='flex flex-row w-full h-full'>
+      <div className='flex flex-col justify-center container'>
+        <div className="home-container m-40">
+          <h1>Gem.edu</h1>
+          <p>Your personal teacher and advisor outside of the classroom.</p>
+          {user ? <SignOutButton /> : <SignInButton />}
+        </div>
+      </div>
+      <div className='flex justify-center items-center w-full'>
+        <img className="logo" src={logo} alt="Logo" />
       </div>
     </div>
   );
